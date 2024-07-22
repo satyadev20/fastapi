1927933bb7286caba875a467cd375d409d3709c0
satyadev20 <garikapatisrisatyadev@gmail.com>
2024-07-22 16:28:28 +0000

    a

--- main.py
+++ main.py
@@ -6,14 +6,11 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     print("in root")
-    time.sleep(10000)
+    time.sleep(200)
     return 'done'
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
 
 @app.on_event("shutdown")
 def fun():
     print("In Shutdown")
-    time.sleep(10)
+    time.sleep(100)
+
